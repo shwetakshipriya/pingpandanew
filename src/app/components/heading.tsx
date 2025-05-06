c7f7310636493080e2d784acdeddd5cd95e53ca1
Shwetakshi Priya <110232435+shwetakshipriya@users.noreply.github.com>
2025-05-06 19:19:46 +0530

    landing page and reusable components

---
+++ src/app/components/heading.tsx
@@ -0,0 +1,20 @@
+import { ReactNode, HTMLAttributes } from "react"
+import { cn } from "@/utils"
+
+interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
+  children?: ReactNode
+}
+
+export const Heading = ({ children, className, ...props }: HeadingProps) => {
+  return (
+    <h1
+      className={cn(
+        "text-4xl sm:text-5xl text-pretty font-heading font-semibold tracking-tight text-zinc-800",
+        className
+      )}
+      {...props}
+    >
+        {children}
+    </h1>
+  )
+}
---
+++ src/app/components/max-width-wrapper.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react"
+import { cn } from "@/utils"
+interface MaxWidthWrapperProps {
+  className?: string
+  children: ReactNode
+}
+export const MaxWidthWrapper = ({
+  className,
+  children,
+}: MaxWidthWrapperProps) => {
+  return (
+    <div
+      className={cn(
+        "h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20 ",
+        className
+      )}
+    >
+        {children}
+    </div>
+  )
+}
