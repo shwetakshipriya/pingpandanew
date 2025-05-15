306fb77230d51637c3c712477c12a12fe7768908
Shwetakshi Priya <110232435+shwetakshipriya@users.noreply.github.com>
2025-05-15 11:43:42 +0530

    created advanced table

--- src/server/routers/category-router.ts
+++ src/server/routers/category-router.ts
@@ -170,6 +170,80 @@ export const categoryRouter = router({
 
       return c.json({ hasEvents })
     }),
+
+    getEventsByCategoryName: privateProcedure
+    .input(
+      z.object({
+        name: CATEGORY_NAME_VALIDATOR,
+        page: z.number(),
+        limit: z.number().max(50),
+        timeRange: z.enum(["today", "week", "month"]),
+      })
+    )
+     .query(async ({ c, ctx, input }) => {
+      const { name, page, limit, timeRange } = input
+
+      const now = new Date()
+      let startDate: Date
+
+      switch (timeRange) {
+        case "today":
+          startDate = startOfDay(now)
+          break
+        case "week":
+          startDate = startOfWeek(now, { weekStartsOn: 0 })
+          break
+        case "month":
+          startDate = startOfMonth(now)
+          break
+      }
+        const [events, eventsCount, uniqueFieldCount] = await Promise.all([
+        db.event.findMany({
+          where: {
+            EventCategory: { name, userId: ctx.user.id },
+            createdAt: { gte: startDate },
+          },
+          skip: (page - 1) * limit,
+          take: limit,
+          orderBy: { createdAt: "desc" },
+        }),
+        db.event.count({
+          where: {
+            EventCategory: { name, userId: ctx.user.id },
+            createdAt: { gte: startDate },
+          },
+        }),
+        db.event
+          .findMany({
+            where: {
+              EventCategory: { name, userId: ctx.user.id },
+              createdAt: { gte: startDate },
+            },
+            select: {
+              fields: true,
+            },
+            distinct: ["fields"],
+          })
+          .then((events) => {
+            const fieldNames = new Set<string>()
+            events.forEach((event) => {
+              Object.keys(event.fields as object).forEach((fieldName) => {
+                fieldNames.add(fieldName)
+              })
+            })
+            return fieldNames.size
+          }),
+      ])
+
+       return c.superjson({
+        events,
+        eventsCount,
+        uniqueFieldCount,
+      })
+    }),
+
+    
+
    
 
 
